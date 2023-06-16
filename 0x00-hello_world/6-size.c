b8e39040e4de37b72a09b8805564c497c5a217aa
Paschal2 <oganablaise@gmail.com>
2023-06-15 18:07:53 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various comp
+ * main - Entry point
  * Return: 0 (Success)
  */
 int main(void)
 {
-printf("Size of char: %lu byte(s)\n", sizeof(char));
-printf("Size of int: %lu byte(s)\n", sizeof(int));
-printf("Size of long: %lu byte(s)\n", sizeof(long int));
-printf("Size of long long: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
