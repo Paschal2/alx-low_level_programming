e6896654ed81097d53e30cd76a2866ba96ab80f6
Paschal2 <oganablaise@gmail.com>
2023-06-16 21:37:11 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (letter != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
