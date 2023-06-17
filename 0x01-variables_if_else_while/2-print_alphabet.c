ee5765b92a3802cfd4a670bcdaf0e8af0ffd4e60
Paschal2 <oganablaise@gmail.com>
2023-06-16 19:50:36 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,14 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char c;
 
-	for (i = 0; i < 26; i++)
-		putchar(alp[i])
-			putchar("\n");
+	c = "a";
+	while (c <= "z")
+	{
+		putchar(c);
+		c++;
+	}
+	putchar("\n");
 	return (0);
 }
