455e6eef7b7cb59283856e8a14e063f39b9c90b9
Paschal2 <oganablaise@gmail.com>
2023-06-16 23:00:24 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A program that prints combinations
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
