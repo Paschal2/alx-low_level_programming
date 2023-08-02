79876bb7fcfaddc66dbeb7b14c93d40de702d772
Paschal2 <oganablaise@gmail.com>
2023-08-01 19:39:00 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,35 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include "lists.h"
 
-// Define the custom data structure for the linked list
-typedef struct listint_t {
-    int value;
-    struct listint_t* next;
-} listint_t;
+/**
+ * main - check the code
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    listint_t *new;
+    listint_t hello = {8, NULL};
+    size_t n;
 
-// Function to print all elements of the linked list
-void print_list(listint_t* head) {
-    listint_t* current = head;
-
-    while (current != NULL) {
-        printf("%d ", current->value);
-        current = current->next;
+    head = &hello;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
     }
-
-    printf("\n");
-}
-
-// Example usage
-int main() {
-    // Create a sample linked list: 1 -> 2 -> 3 -> NULL
-    listint_t node1 = {1, NULL};
-    listint_t node2 = {2, NULL};
-    listint_t node3 = {3, NULL};
-
-    node1.next = &node2;
-    node2.next = &node3;
-
-    // Print the linked list
-    print_list(&node1);
-
-    return 0;
+    new->n = 9;
+    new->next = head;
+    head = new;
+    n = print_listint(head);
+    printf("-> %lu elements\n", n);
+    free(new);
+    return (0);
 }
